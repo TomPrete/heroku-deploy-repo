b1fe813342f4e9bde7637642931bb8f098d29d8d
Tom Prete <prete316@gmail.com>
2021-04-08 11:17:58 -0500

    make it better

---
+++ wines/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Wine
+
+
+class WineForm(forms.ModelForm):
+  class Meta:
+    model = Wine
+    fields = ['wine_name', 'price', 'varietal', 'description']
