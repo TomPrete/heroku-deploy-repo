90dcbc1fb56e70c7f6f5800848a94c4766a42fd8
Tom Prete <prete316@gmail.com>
2021-04-08 17:07:29 -0500

    added a comment

--- wines/forms.py
+++ wines/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Wine
 
 # Here is the Model Form
+# Adding a comment here in the forms file
 class WineForm(forms.ModelForm):
   class Meta:
     model = Wine
